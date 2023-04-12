9f1a4f4aee580bb1491051e3c000cf07a1bd2996
sksjw95 <tjwldnd95@hanmail.net>
2023-04-13 00:13:06 +0900

    fetch: api

---
+++ src/main/java/com/example/ToDo/App/TodoController.java
@@ -0,0 +1,14 @@
+package com.example.ToDo.App;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TodoController {
+    @GetMapping
+    public String helloWorld() {
+        return "To-do Application !";
+    }
+}
